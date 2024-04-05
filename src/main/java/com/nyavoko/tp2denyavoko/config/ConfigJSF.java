b69598b72540483039c623924b877e04b07577e5
Avoko Nivoarison <avokonivoarison@gmail.com>
2024-04-05 11:54:57 +0300

    tp2 finished

---
+++ src/main/java/com/nyavoko/tp2denyavoko/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nyavoko.tp2denyavoko.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+/**
+ *
+ * @author Avoko
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/com/nyavoko/tp2denyavoko/jsf/Bean2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.nyavoko.tp2denyavoko.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named(value = "bean2") 
+@RequestScoped
+public class Bean2 {
+
+    private int nombre;
+    private int nombrePrecedent;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+    
+    // Méthode pour retourner à la page formulaire_4 avec le nombre précédent
+    public String retournerFormulaire() {
+        return "formulaire_4?nombre=" + nombrePrecedent + "&amp;faces-redirect=true";
+    }
+    
+    // Méthode pour afficher les nombres suivants
+    public String afficher() {
+        nombrePrecedent = nombre; 
+        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
